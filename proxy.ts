340ddc2093bbcd06c584d408dca62b55a84f89b1
Sedi98 <sedi9816@gmail.com>
2026-04-06 16:14:43 +0400

    fixed default lang

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,6 @@
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { i18n } from "@/i18n/config";
-
-function getLocale(request: NextRequest) {
-  const headers = Object.fromEntries(request.headers.entries());
-  const languages = new Negotiator({ headers }).languages();
-
-  return match(languages, i18n.locales, i18n.defaultLocale);
-}
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -20,8 +11,7 @@ export function proxy(request: NextRequest) {
 
   if (pathnameHasLocale) return;
 
-  const locale = getLocale(request);
-  request.nextUrl.pathname = `/${locale}${pathname}`;
+  request.nextUrl.pathname = `/${i18n.defaultLocale}${pathname}`;
 
   return NextResponse.redirect(request.nextUrl);
 }
